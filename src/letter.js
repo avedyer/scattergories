d0457c64c472d2ad64254b28f956e20cc45ba9ef
avedyer <avedyer@gmail.com>
2022-07-15 10:33:15 -0400

    letter randomizer

--- src/letter.js
+++ src/letter.js
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 export default function Letter() {
+  const [letter, setLetter] = useState(randomLetter())
+
+  function randomLetter() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return letters[Math.floor(Math.random() * letters.length)]
+  }
+
   return (
     <div id='letter'>
-
+      <button id='reroll' onClick={() => setLetter(randomLetter())}/>
+      <span>{letter}</span>
     </div>
   )
 }
