56515282b34f73c6cd2d8f6b0562040a1105a396
avedyer <avedyer@gmail.com>
2022-07-21 08:41:13 -0400

    moved list importing up to categories module for better communication between list and editor modules"

--- src/categories.js
+++ src/categories.js
@@ -1,9 +1,47 @@
+import { useState, useEffect } from "react"
+
 import List from "./list"
+import Editor from './editor'
+
+import prompts from './prompts.txt'
 
 export default function Categories(props) {
-  return (
+
+  const [editing, setEditing] = useState(false)
+  const [fullList, setfullList] = useState([])
+
+  useEffect(() => {
+    if (fullList.length === 0) {
+      fetch(prompts)
+      .then(r => r.text())
+      .then(text => {
+        let newArr = []
+        let queuedPrompt = ''
+        for (let i=0; i<text.length; ++i) {
+          if (text[i] !== ',') {
+            queuedPrompt += text[i]
+          }
+          else {
+            newArr.push(queuedPrompt)
+            queuedPrompt = ''
+          }
+        }
+        newArr.push(queuedPrompt)
+        setfullList([...newArr])
+      });
+    }
+  }, [])
+
+  if (!editing) {
+    return (
+      <div id='categories'>
+        <List fullList={fullList}/>  
+      </div>
+    )
+  }
+  else {
     <div id='categories'>
-      <List />  
+      <Editor fullList={fullList}/>
     </div>
-  )
+  }
 }
--- src/list.js
+++ src/list.js
@@ -1,46 +1,24 @@
 import { useEffect, useState } from 'react';
-import prompts from './prompts.txt'
+
 
 export default function List(props) {
 
-  const [fullList, setfullList] = useState([])
+  
   const [promptList, setPromptList] = useState([])
   const [promptCount, setPromptCount] = useState(10)
   const [exclusions, setExclusions] = useState([])
 
   useEffect(() => {
-    if (fullList.length === 0) {
-      fetch(prompts)
-      .then(r => r.text())
-      .then(text => {
-        let newArr = []
-        let queuedPrompt = ''
-        for (let i=0; i<text.length; ++i) {
-          if (text[i] !== ',') {
-            queuedPrompt += text[i]
-          }
-          else {
-            newArr.push(queuedPrompt)
-            queuedPrompt = ''
-          }
-        }
-        newArr.push(queuedPrompt)
-        setfullList([...newArr])
-      });
-    }
-  }, [])
-
-  useEffect(() => {
-    if (promptList.length < promptCount && fullList.length > 0) {
+    if (promptList.length < promptCount && props.fullList.length > 0) {
       let newArr = [...promptList]
       newArr.push(randomPrompt())
       setPromptList([...newArr])
     }
-  }, [fullList , promptList])
+  }, [props.fullList , promptList])
 
   function randomPrompt() {
 
-    let newPrompt = fullList[Math.floor(Math.random() * fullList.length)]
+    let newPrompt = props.fullList[Math.floor(Math.random() * props.fullList.length)]
     
     if (exclusions.includes(newPrompt) || promptList.includes(newPrompt)) {
       return randomPrompt()
