9b7601249181eaea91cd7703ccb3d64f5c9100d0
avedyer <avedyer@gmail.com>
2022-07-22 07:49:16 -0400

    new category submission through enter key event

--- src/editor.js
+++ src/editor.js
@@ -4,10 +4,17 @@ export default function Editor(props) {
 
   const [newCategory, setNewCategory] = useState()
 
+  function checkSubmit(e) {
+    if (e.key === 'Enter') {
+      props.passNewCategory(e.target.value)
+      e.target.value = ''
+    }
+  }
+
   return (
     <div id='editing-container'>
       <div id='category-form'>
-        <input type='text' onChange={(e) => setNewCategory(e.target.value)}/>
+        <input type='text' onKeyUp={(e) => checkSubmit(e)} onChange={(e) => setNewCategory(e.target.value)}/>
         <button onClick={() => props.passNewCategory(newCategory)}>Add</button>
       </div>
       <button onClick={() => props.passEditing(false)}>Save</button>
